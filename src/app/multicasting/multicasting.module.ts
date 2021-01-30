e2f41934316ed95255f50f40ac165eedcddb8889
Mohan Nalgire <mohan.nalgire@ibm.com>
2021-01-30 15:39:19 +0530

    Added multicasting sub links

--- src/app/multicasting/multicasting.module.ts
+++ src/app/multicasting/multicasting.module.ts
@@ -3,8 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { MulticastingRoutingModule } from './multicasting-routing.module';
 
+import { MulticastComponent } from './multicast/multicast.component';
+import { MulticastingComponent } from './multicasting/multicasting.component';
+import { PublishBehaviorComponent } from './publish-behavior/publish-behavior.component';
+import { PublishLastComponent } from './publish-last/publish-last.component';
+import { PublishReplayComponent } from './publish-replay/publish-replay.component';
+import { PublishComponent } from './publish/publish.component';
+import { ShareComponent } from './share/share.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    MulticastComponent,
+    MulticastingComponent,
+    PublishBehaviorComponent,
+    PublishLastComponent,
+    PublishReplayComponent,
+    PublishComponent,
+    ShareComponent,
+    PublishBehaviorComponent,
+  ],
   imports: [CommonModule, MulticastingRoutingModule],
 })
 export class MulticastingModule {
