82d7e833bcf19ea1369b325fd4e6d91e5af25176
Mohan Nalgire <mohan.nalgire@ibm.com>
2021-02-14 15:08:07 +0530

    updated code for distinct-until-changed

--- src/app/filtering/distinct-until-key-changed/distinct-until-key-changed.component.ts
+++ src/app/filtering/distinct-until-key-changed/distinct-until-key-changed.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import { fromEvent, of } from 'rxjs';
+import { distinctUntilKeyChanged, pluck } from 'rxjs/operators';
 
 @Component({
   selector: 'app-distinct-until-key-changed',
@@ -6,10 +9,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./distinct-until-key-changed.component.css']
 })
 export class DistinctUntilKeyChangedComponent implements OnInit {
-
-  constructor() { }
+  tasks = [
+    {
+      id: 1,
+      title: 'task1',
+      status: 'done'
+    },
+    {
+      id: 2,
+      title: 'task2',
+      status: 'inprogres'
+    },
+    {
+      id: 3,
+      title: 'task3',
+      status: 'complete'
+    },
+    {
+      id: 4,
+      title: 'task4',
+      status: 'assigned'
+    },
+    {
+      id: 5,
+      title: 'task5',
+      status: 'assigned'
+    }
+  ];
+  distinctStatuses=[];
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
+    /**
+     *    distinctUntilKeyChanged - To filter distinct status from the array data
+     *    pluck - To fetch only status column data from array data
+     */
+    from(this.tasks)
+        .pipe(
+          distinctUntilKeyChanged('status'),
+          pluck('status'),
+        ).subscribe(
+          data=>{
+            console.log('test on load', data);
+            this.distinctStatuses.push(data);
+          }
+        );
+
   }
 
 }
